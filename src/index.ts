5244ce537e1213c9afaef1236c1c7402c0376415
ShompiFlen <shompire02@gmail.com>
2026-05-24 21:07:47 -0400

    exported types from index

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,43 @@ export const TMLAPI = {
   ranking: RankingsAPI,
   coaching: CoachingAPI,
 }
+
+export type {
+  APICoach,
+  APICoachCreationData,
+  APICoachSocial,
+  APICoachUpdateData,
+  APICoachingTicket,
+  APICoachingTicketCreationData,
+  APICoachingTicketPatchData,
+  APISocialCreationData,
+  APIStudent,
+  APIStudentCreationData,
+} from "./api/coaching.ts"
+
+export type {
+  APIEvent,
+  EventCreationData,
+  EventPatchData,
+  EventsQueryParams
+} from "./api/events.ts"
+
+export type {
+  BaseCountryLeaderboard,
+  BaseLeaderboardPlayer,
+  CountryLeaderboard,
+  LeaderboardPlayer,
+  LeaderboardSums,
+  LeaderboardVariations,
+  RankingLeaderboardResponse,
+  RankingQueryParams
+} from "./api/rankings.ts"
+export type {
+
+} from "./api/status.ts"
+export type {
+  APIWebpageEditData,
+  APIWebpagePostCreationData,
+  APIWebpagePost,
+  WebpagePostsQueryParams
+} from "./api/webposts.ts"
